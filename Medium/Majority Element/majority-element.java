3f72024c20858be895d14dae7a8ec5d3163eb113
Gayatri <63839136+gayatri-22@users.noreply.github.com>
2023-09-22 22:07:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Majority Element/majority-element.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int a[], int size)
+    {
+        // your code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num:a){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        
+        for(int n:a){
+            if(map.get(n)>size/2){
+                return n;
+            }
+        }
+        return -1;
+    }
+}
